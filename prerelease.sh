2dabb7edc0e16dd29f1ef863aa81d1e00a487e16
Katherine Stanley <katheris@uk.ibm.com>
2017-04-20 17:45:23 +0100

    Move prerelease..sh invocation to inside if statement.
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- prerelease.sh
+++ prerelease.sh
@@ -1,8 +1,12 @@
-
 git config user.email "travisci@travis.ibm.com"
 git config user.name "Travis CI"
 git config push.default simple
-git checkout -b addCodeCoverageFiles
+echo "Revving version"
+npm run release
+echo "Creating git branch"
+PKG_VER_NEXT=`node -e "console.log(require('./package.json').version);"`
+BRANCH="updateTo${PKG_VER_NEXT}"
+git checkout -b $BRANCH
 ls -l
 echo "Creating ../docs"
 mkdir -p ../docs/cc
@@ -12,9 +16,11 @@ echo "Copying ./coverage to ../docs"
 cp -r ./coverage/* ../docs/cc
 ls -l ../docs/cc
 ../scan.sh
+echo "Copying scan results file to ../docs"
 cp ./production/java-codegen-yeoman/generator-java/generator-java_scanresults* ../docs/scan
 git status
 git add ../docs
 git status
-git commit -m "Update test coverage files"
-git push origin addCodeCoverageFiles
+git commit -m "Update test coverage and code scan files"
+#this branch will need to be reviewed and approved in the usual manner
+git push --follow-tags origin $BRANCH
--- scan.sh
+++ scan.sh
@@ -4,4 +4,5 @@ git clone git@github.ibm.com:arf/java-codegen-yeoman.git
 cd ./java-codegen-yeoman/generator-java
 npm install --only=production
 npm install @ibm/osc-cli -g
+echo "Running scan"
 osc-cli -s . -o -u travisci@travis.ibm.com
