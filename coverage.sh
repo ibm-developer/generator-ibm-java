ba82cfb1128666b3d8f2b49e9ea71b40215f724c
Travis CI <travisci@travis.ibm.com>
2017-04-24 16:04:59 +0100

    feat(infrastructure>): Make shell script files cleaner
    Remove extra mkdir line and use variable to change directories.
    
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,3 @@
-echo "Creating ../docs/cc"
-mkdir -p ../docs/cc
 echo "Creating ../docs/cc/unit"
 mkdir -p ../docs/cc/unit
 echo "Running unit test coverage"
@@ -9,6 +7,7 @@ cp -r ./coverage/* ../docs/cc/unit
 ls -l ../docs/cc/unit
 echo "Creating ../docs/cc/int"
 mkdir -p ../docs/cc/int
+rm -rf ./coverage
 echo "Running integration test coverage"
 npm run coverageint
 echo "Copying ./coverage to ../docs/cc/int"
