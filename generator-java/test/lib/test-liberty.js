dfbb03f53937842cd2a339338aa74c0331445959
Travis CI <travisci@travis.ibm.com>
2017-06-07 10:12:52 +0100

    fix(templates): Update Liberty test to conditionally check server.xml
    Update Liberty tests to conditionally check features in the server.xml
    
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- generator-java/test/lib/test-liberty.js
+++ generator-java/test/lib/test-liberty.js
@@ -84,7 +84,7 @@ test_liberty.prototype.assertFeatures = function() {
     if (arguments[i] && typeof arguments[i] === 'string') {
       var feature = arguments[i];
       it('server.xml contains <feature>' + feature + '</feature>', function() {
-        assert.fileContent(LIBERTY_CONFIG_FILE, "<feature>" + feature + "</feature>");
+        check(LIBERTY_CONFIG_FILE, "<feature>" + feature + "</feature>");
       });
     }
   }
