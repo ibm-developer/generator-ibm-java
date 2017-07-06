43e30c9cff09cbcad68383f036eb7212c1bf4e97
MarkNSweep <MarkNSweep@github.com>
2017-07-06 16:01:13 +0100

    add control file for common template folder

--- generator-java/generators/app/templates/basic/control.js
+++ generator-java/generators/app/templates/basic/control.js
@@ -34,18 +34,5 @@
     "services/objectStorage",
     {{/objectStorage}}
     {{/bluemix}}
-  ],
-  fileFound : function(path, contents, config) {
-    var defaultFragment = {path : path, template : contents, data : config};
-    if(path == 'gitignore') {
-      var fragment = {
-        path : '.gitignore',
-        template : contents,
-        data : config
-      }
-      return [fragment];
-    } else {
-      return [defaultFragment];
-    }
-  }
+  ]
 }
