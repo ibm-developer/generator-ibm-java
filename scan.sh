c3017d38d9526ce169cd340cb2e5cb1ec026fcda
MarkNSweep <MarkNSweep@github.com>
2017-07-31 11:59:22 +0100

    fix(infrastructure): update tool for oss scans

--- scan.sh
+++ scan.sh
@@ -6,9 +6,9 @@ cd ./production
 git clone git@github.ibm.com:arf/java-codegen-yeoman.git
 cd ./java-codegen-yeoman/generator-java
 npm install --only=production
-npm install @ibm/osc-cli -g
+npm install @wicked/cli -g
 echo "Running scan"
-osc-cli -s . -o -u travisci@travis.ibm.com
+wicked-cli -s . -u travisci@travis.ibm.com
 cd $WORKING_DIR
 echo "Copying scan results file to ../docs"
-cp ./production/java-codegen-yeoman/generator-java/generator-java_scanresults* ../docs/scan
+cp ./production/java-codegen-yeoman/generator-java/generator-java_scan-results/* ../docs/scan
