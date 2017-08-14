a168bfd2e6ecf9febc682396086ab7defb60808f
Travis CI <travisci@travis.ibm.com>
2017-08-14 12:21:05 +0100

    fix(templates): Move helm charts to chart/appname
    Update control.js to write out helm charts to chart/appname.toLowerCase()
    and update cli-config.yml to reflect new location. Remove extra image name
    template from deployment.yaml.
    
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- generator-java/generators/app/templates/platform/kube/control.js
+++ generator-java/generators/app/templates/platform/kube/control.js
@@ -3,5 +3,13 @@
     {{#has frameworkType 'spring'}}
     "Jenkinsfile"
     {{/has}}
-  ]
+  ],
+  {{! take helm files in chart directory and write to chart/appName.toLowerCase() in generated project }}
+  fileFound : function(path, contents, config) {
+    var defaultFragment = {path : path, template : contents, data : config};
+    if(path.startsWith('chart')) {
+      defaultFragment.path = path.replace('chart', 'chart/' + config.appName.toLowerCase());
+    }
+    return [defaultFragment];
+  }
 }
