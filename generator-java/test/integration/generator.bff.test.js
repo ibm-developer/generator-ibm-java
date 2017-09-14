acaa04d638ee53f1196ed6e565f36d6d29a17ebd
Travis CI <travisci@travis.ibm.com>
2017-09-14 10:13:17 +0100

    feat(templates): Add Spring templates for BFF
    Add Spring templates for Backend for Frontend pattern.
    
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- generator-java/test/integration/generator.bff.test.js
+++ generator-java/test/integration/generator.bff.test.js
@@ -25,10 +25,12 @@ const helpers = require('yeoman-test');
 const common = require('../lib/test-common');
 const framework = require('../lib/test-framework');
 const tests = require('@arf/java-common');
+const command = tests.test('command');
 const core = require('../lib/core');
 const extend = require('extend');
 
 const FRAMEWORK_LIBERTY = 'liberty';
+const FRAMEWORK_SPRING = 'spring';
 
 class Options extends core.BxOptions {
   constructor(runHeadless, buildType, frameworkType, name) {
@@ -45,42 +47,56 @@ class Options extends core.BxOptions {
 
   assert(appName, ymlName, cloudant, objectStorage, buildType, frameworkType) {
     super.assert(appName, ymlName, cloudant, objectStorage, 'bff');
-    common.assertFiles('src', true, 'main/java/application/rest/SwaggerEndpoint.java',
-                                    'main/java/application/model/Product.java',
-                                    'main/java/application/openapi/ProductsApi.java',
-                                    'main/java/application/openapi/ProductApi.java',
-                                    'test/java/it/ProductsEndpointTest.java',
-                                    'test/java/it/SwaggerEndpointTest.java');
     framework.test(frameworkType).assertSourceFiles(false);
     frameworkType === FRAMEWORK_LIBERTY ? this.assertliberty() : this.assertspring();
-    if(buildType === 'maven') {
-      tests.test(buildType).assertContent('<feature>apiDiscovery-1.0</feature>');
-    }
-    if(buildType === 'gradle') {
-      tests.test(buildType).assertContent("name = ['apiDiscovery-1.0']");
-    }
   }
 
   assertliberty() {
     super.assertliberty();
+    common.assertFiles('src', true, 'main/java/application/rest/SwaggerEndpoint.java',
+                                'main/java/application/model/Product.java',
+                                'main/java/application/openapi/ProductsApi.java',
+                                'main/java/application/openapi/ProductApi.java',
+                                'test/java/it/ProductsEndpointTest.java',
+                                'test/java/it/SwaggerEndpointTest.java');
     var test = tests.test(this.values.buildType);
     test.assertDependency('provided', 'io.swagger', 'swagger-annotations', '1.5.3');
     test.assertDependency('provided', 'javax.ws.rs', 'javax.ws.rs-api', '2.0.1');
     test.assertDependency('provided', 'com.ibm.websphere.appserver.api', 'com.ibm.websphere.appserver.api.jaxrs20', '1.0.10');
     framework.test(FRAMEWORK_LIBERTY).assertFeatures('jaxrs-2.0');
     framework.test(FRAMEWORK_LIBERTY).assertFeatures('apiDiscovery-1.0');
+    if(this.values.buildType === 'maven') {
+      test.assertContent('<feature>apiDiscovery-1.0</feature>');
+    }
+    if(this.values.buildType === 'gradle') {
+      test.assertContent("name = ['apiDiscovery-1.0']");
+    }
   }
 
   assertSpring() {
     super.assertSpring();
+    common.assertFiles('src', true, 'main/java/io/swagger/api/ProductApi.java',
+                                    'main/java/io/swagger/api/ProductApiController.java',
+                                    'main/java/io/swagger/api/ProductsApi.java',
+                                    'main/java/io/swagger/api/ProductsApiController.java',
+                                    'main/java/io/swagger/model/Product.java');
+    framework.test(FRAMEWORK_SPRING).assertOpenApi(true, [], this.values.buildType);
+    var test = tests.test(this.values.buildType);
+    test.assertDependency('compile', 'org.springframework.boot', 'spring-boot-starter-web');
+    test.assertDependency('compile', 'org.springframework.boot', 'spring-boot-actuator');
+    test.assertDependency('test', 'org.springframework.boot', 'spring-boot-starter-test');
+  }
+
+  assertCompiles(buildType) {
+    command.run(tests.test(buildType).getCompileCommand());
   }
 
 }
 
-var frameworkTypes = ['liberty'];
+var frameworkTypes = ['liberty', 'spring'];
 
 describe('java generator : bff integration test', function () {
-  this.timeout(7000);
+  this.timeout(10000);
   frameworkTypes.forEach(frameworkType => {
     describe('Generates a basic bff project (no bluemix), gradle build with prompts', function () {
       var options = new Options(false, 'gradle', frameworkType);
@@ -100,12 +116,14 @@ describe('java generator : bff integration test', function () {
       var options = new Options(true, 'gradle', frameworkType, core.APPNAME);
       before(options.before.bind(options));
       options.assert(core.APPNAME, core.APPNAME, false, false, 'gradle', frameworkType);
+      options.assertCompiles('gradle');
     });
 
     describe('Generates a basic bff project (no bluemix), maven build', function () {
       var options = new Options(true, 'maven', frameworkType, core.APPNAME);
       before(options.before.bind(options));
       options.assert(core.APPNAME, core.APPNAME, false, false, 'maven', frameworkType);
+      options.assertCompiles('maven');
     });
 
     describe('Generates a basic bff project (bluemix) with cloudant', function () {
@@ -116,6 +134,7 @@ describe('java generator : bff integration test', function () {
       before(options.before.bind(options));
       options.assert('bxName', 'bxName', true, false, 'maven', frameworkType);
       options.assertCloudant();
+      options.assertCompiles('maven');
     });
 
     describe('Generates a basic bff project (bluemix) with Object Storage', function () {
@@ -126,6 +145,7 @@ describe('java generator : bff integration test', function () {
       before(options.before.bind(options));
       options.assert('bxName', 'bxName', false, true, 'maven', frameworkType);
       options.assertObjectStorage();
+      options.assertCompiles('maven');
     });
 
     describe('Generates a basic bff project (bluemix) with Cloudant and Object Storage', function () {
