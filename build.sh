b28d16c1975862d7e4408dc0fdad92ec175bb684
MarkNSweep <MarkNSweep@github.com>
2017-09-29 11:33:54 +0100

    invert pr check

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ echo "Build script : version 0.0.1"
 echo "Checking if this a PR to development"
 if [[ $TRAVIS_BRANCH == "development"  ]]; then
   echo "Build targetting development - checking if this is a PR or not"
-  if [[ $TRAVIS_PULL_REQUEST == "true" ]]; then
-    echo "Development PR detected, running all tests"
+  if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
+    echo "Development build detected, running all tests"
     echo "Unit tests"
     npm run test
     if [ $? != 0 ]; then
