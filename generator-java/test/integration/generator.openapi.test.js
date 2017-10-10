a072403ede6be33d6986feb21565f4a99b96b2bf
MarkNSweep <MarkNSweep@github.com>
2017-10-10 22:06:23 +0100

    update tests for now example test message

--- generator-java/test/integration/generator.openapi.test.js
+++ generator-java/test/integration/generator.openapi.test.js
@@ -121,7 +121,7 @@ function execute(frameworkType) {
       options.assertCompiles();
 
       it('should create a basic microservice, gradle build system', function () {
-        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("Some data");'); //check no bx services present
+        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("Congratulations, your application is up and running");'); //check no bx services present
         assert.fileContent('README.md', 'gradle');
         assert.noFileContent('README.md', 'maven');
       });
@@ -140,7 +140,7 @@ function execute(frameworkType) {
       options.assertCompiles();
 
       it('should create a basic microservice, maven build system', function () {
-        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("Some data");'); //check no bx services present
+        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("SCongratulations, your application is up and running");'); //check no bx services present
         assert.fileContent('README.md', 'maven');
         assert.noFileContent('README.md', 'gradle');
       });
@@ -161,7 +161,7 @@ function execute(frameworkType) {
       options.assertCompiles();
 
       it('should create a basic microservice, gradle build system', function () {
-        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("Some data");'); //check no bx services present
+        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("Congratulations, your application is up and running");'); //check no bx services present
         assert.fileContent('README.md', 'gradle');
         assert.noFileContent('README.md', 'maven');
       });
@@ -182,7 +182,7 @@ function execute(frameworkType) {
       options.assertCompiles();
 
       it('should create a basic microservice, maven build system', function () {
-        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("Some data");'); //check no bx services present
+        assert.fileContent('src/main/java/application/rest/v1/Example.java','list.add("Congratulations, your application is up and running");'); //check no bx services present
         assert.fileContent('README.md', 'maven');
         assert.noFileContent('README.md', 'gradle');
       });
