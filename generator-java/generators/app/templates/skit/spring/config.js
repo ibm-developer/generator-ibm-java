ca74f7b6825c7454ecf0aacae130c0da97e9821c
MarkNSweep <MarkNSweep@github.com>
2017-11-13 17:23:42 +0000

    minor review corrections

--- generator-java/generators/app/templates/skit/spring/config.js
+++ generator-java/generators/app/templates/skit/spring/config.js
@@ -1,7 +1,6 @@
 {
   "dependencies" : [
     {"groupId" : "org.springframework.boot", "artifactId" : "spring-boot-actuator"},
-    {"groupId" : "org.springframework.cloud", "artifactId" : "spring-cloud-starter-hystrix"},
     {"groupId" : "org.springframework.boot", "artifactId" : "spring-boot-starter-test", "scope" : "test"}
   ]
 }
