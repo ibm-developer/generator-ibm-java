93153ad4ac89edb8e6c6c13dccea8a51beecd185
Paul W Bennett <pwbennet@us.ibm.com>
2017-11-17 15:43:53 -0600

    fix(templates): use Mongo CA cert for Spring microservice - arf/planning-java/issues/283

--- generator-java/generators/app/templates/blank/spring/src/main/java/application/SBApplication.java
+++ generator-java/generators/app/templates/blank/spring/src/main/java/application/SBApplication.java
@@ -2,8 +2,20 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+{{#bluemix}}
+{{#mongodb}}
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+{{/mongodb}}
+{{/bluemix}}
 
 @SpringBootApplication
+{{#bluemix}}
+{{#mongodb}}
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+{{/mongodb}}
+{{/bluemix}}
 public class SBApplication {
 
     public static void main(String[] args) {
