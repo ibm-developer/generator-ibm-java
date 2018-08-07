13ea1bcf2d96f326727d081e3ab2e33ef9c81562
Paul W Bennett <pwbennet@us.ibm.com>
2018-08-07 12:06:16 -0500

    Test develop branch build - just npm test

--- build.sh
+++ build.sh
@@ -18,23 +18,23 @@ retval=$?
 if [ $retval != 0 ]; then
   exit $retval
 fi
-echo "Running integration tests"
-npm run testint
-retval=$?
-if [ $retval != 0 ]; then
-  exit $retval
-fi
-echo "Running coveralls"
-npm run coveralls
-echo "Running common tests"
-npm run testcommon
-retval=$?
-if [ $retval != 0 ]; then
-  exit $retval
-fi
-echo "Running end to end tests"
-npm run teste2e
-retval=$?
-if [ $retval != 0 ]; then
-  exit $retval
-fi
+# echo "Running integration tests"
+# npm run testint
+# retval=$?
+# if [ $retval != 0 ]; then
+#   exit $retval
+# fi
+# echo "Running coveralls"
+# npm run coveralls
+# echo "Running common tests"
+# npm run testcommon
+# retval=$?
+# if [ $retval != 0 ]; then
+#   exit $retval
+# fi
+# echo "Running end to end tests"
+# npm run teste2e
+# retval=$?
+# if [ $retval != 0 ]; then
+#   exit $retval
+# fi
