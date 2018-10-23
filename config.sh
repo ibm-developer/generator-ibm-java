0827e8f9863b6ee6c7683c53d1f360e9a2aad536
Christian Compton <cbcompto@us.ibm.com>
2018-10-23 16:28:55 -0500

    chore: new devops versioning workflow

--- config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-CMD=$(cat .git/config | grep "commit")
-if [ -z $CMD ]; then
-   echo "$(cat .git/config)\n$(cat .gitconfig)" > ./.git/config
-else
-    echo "commit file template already saved”
-fi
